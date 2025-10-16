8224426d21052203130312312e141c1aed4826e3
SHSong99 <99pyues2@gmail.com>
2025-10-16 11:48:11 +0900

    release : 재배포

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -7,7 +7,7 @@ cd /home/ubuntu/myapp
 # compose.yml 파일의 image: 속성으로 정의된 모든 서비스의 최신 이미지를 원격 저장소(ECR)에서 한 번에 다 가져옴
 docker compose pull
 
-# 이미지를 빌드(생성)하는게 아니라 ECR에 이미지를 가져와 실행하는것이므로 --build 안붙임
+# 이미지를 빌드(생성)하는게 아니라 ECR에 이미지를 가져와 실행하는것이므로 --build 안붙여야함
 docker compose up -d # 로컬에 해당 이미지가 아예 존재하지 않을 때만 자동으로 pull하므로 이 명령어 전에 pull을 해야 최신버전 이미지를 실행함
 
 echo "--------------- 서버 배포 끝 (Compose & Nginx LB) -----------------"
